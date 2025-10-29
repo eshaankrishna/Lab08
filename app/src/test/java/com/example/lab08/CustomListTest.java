497cc377bc46c0c8edecd2bfc56c3ae584a47548
eshaankrishna <eshaankr@ualberta.ca>
2025-10-29 11:16:22 +0300

    Testing hasCity() method - Fail

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,12 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City city = new City("Edmonton", "Alberta");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+    }
 
 }
